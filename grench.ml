8a0602a305642d4fef9b2e9bf749b15cbc705549
Hugo Duncan <hugo@hugoduncan.org>
2013-08-31 10:35:16 -0400

    Factor out non lein specific code in grench.ml

--- grench.ml
+++ grench.ml
@@ -5,6 +5,51 @@ open Printf
 let splice_args args =
   String.concat ~sep:"\" \"" (List.map args String.escaped)
 
+let main_message ns form session =
+  [("session", session);
+   ("op", "eval");
+   ("id", Uuid.to_string (Uuid.create ()));
+   ("ns", ns);
+   ("code", form)]
+
+let main cwd root ns form port =
+  let message = main_message ns form in
+  Nrepl.new_session "127.0.0.1" port message Repl.handler;
+  never_returns (Scheduler.go ())
+
+let port_err msg =
+  eprintf "%s" msg;
+  Pervasives.exit 1
+
+let repl_port env_var filename port_err_msg =
+  match Sys.getenv env_var with
+    | Some port -> port
+    | None -> match Sys.file_exists filename with
+              | `Yes -> In_channel.read_all filename
+              | `No | `Unknown -> port_err port_err_msg
+
+let rec find_root cwd original =
+  match Sys.file_exists (String.concat ~sep:Filename.dir_sep
+                           [cwd; "project.clj"]) with
+    | `Yes -> cwd
+    | `No | `Unknown -> if (Filename.dirname cwd) = cwd then
+        original
+      else
+        find_root (Filename.dirname cwd) original
+
+let lein_port_err =
+  "Couldn't read port from ~/.lein/repl-port or LEIN_REPL_PORT.\n
+If Leiningen is not running, launch `lein repl :headless' from outside a
+project directory and try again.\n"
+
+let lein_repl_port () =
+  let filename = String.concat
+                   ~sep:Filename.dir_sep [(Sys.getenv_exn "HOME");
+                                          ".lein"; "repl-port"] in
+  repl_port "LEIN_REPL_PORT" filename lein_port_err
+
+let lein_ns = "leiningen.core.main"
+
 let main_form = sprintf "(binding [*cwd* \"%s\", *exit-process?* false]
                            (System/setProperty \"leiningen.original.pwd\" \"%s\")
 
@@ -21,43 +66,9 @@ let main_form = sprintf "(binding [*cwd* \"%s\", *exit-process?* false]
                                  (when-not (and (number? c) (zero? c))
                                    (throw e))))))"
 
-let main_message root cwd args session =
-  [("session", session);
-   ("op", "eval");
-   ("id", Uuid.to_string (Uuid.create ()));
-   ("ns", "leiningen.core.main");
-   ("code", main_form root cwd (splice_args args))]
-
-let port_err () =
-  eprintf "Couldn't read port from ~/.lein/repl-port or LEIN_REPL_PORT.\n
-If Leiningen is not running, launch `lein repl :headless' from outside a
-project directory and try again.\n";
-  Pervasives.exit 1
-
-let repl_port () =
-  match Sys.getenv "LEIN_REPL_PORT" with
-    | Some port -> port
-    | None -> let filename = String.concat
-                ~sep:Filename.dir_sep [(Sys.getenv_exn "HOME");
-                                       ".lein"; "repl-port"] in
-              match Sys.file_exists filename with
-                | `Yes -> In_channel.read_all filename
-                | `No | `Unknown -> port_err ()
-
-let main cwd root args =
-  let port = Int.of_string (repl_port ()) in
-  let message = main_message cwd root args in
-  Nrepl.new_session "127.0.0.1" port message Repl.handler;
-  never_returns (Scheduler.go ())
-
-let rec find_root cwd original =
-  match Sys.file_exists (String.concat ~sep:Filename.dir_sep
-                           [cwd; "project.clj"]) with
-    | `Yes -> cwd
-    | `No | `Unknown -> if (Filename.dirname cwd) = cwd then
-        original
-      else
-        find_root (Filename.dirname cwd) original
+let lein_main cwd root args =
+  let port = Int.of_string (lein_repl_port ()) in
+  main cwd root lein_ns (main_form root cwd (splice_args args)) port
 
 let usage = "usage: grench TASK [ARGS]...
 
@@ -71,8 +82,9 @@ let () =
     match Sys.argv |> Array.to_list |> List.tl with
       | None | Some ["--grench-help"] -> printf "%s\n%!" usage
       | Some ["--leiningen-version"] | Some ["--lein-version"] ->
-        main root cwd ["version"]
+        lein_main root cwd ["version"]
       | Some ["--version"] | Some ["version"] | Some ["-v"] ->
         printf "Grenchman 0.1.0\n%!"
-      | Some ["repl"] -> main root cwd ["run"; "-m"; "clojure.main/main"; "-r"]
-      | Some args -> main root cwd args
+      | Some ["repl"] ->
+         lein_main root cwd ["run"; "-m"; "clojure.main/main"; "-r"]
+      | Some args -> lein_main root cwd args
