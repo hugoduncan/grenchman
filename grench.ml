8b3c5737cbd094bfcb75ffb392537cf232236ce0
Hugo Duncan <hugo@hugoduncan.org>
2013-09-03 08:05:30 -0400

    Fix grench for repl_port returning option

--- grench.ml
+++ grench.ml
@@ -5,13 +5,16 @@ open Printf
 let lein_port_err =
   "Couldn't read port from ~/.lein/repl-port or LEIN_REPL_PORT.\n
 If Leiningen is not running, launch `lein repl :headless' from outside a
-project directory and try again.\n"
+project directory and try again."
 
 let lein_repl_port () =
   let filename = String.concat
                    ~sep:Filename.dir_sep [(Sys.getenv_exn "HOME");
                                           ".lein"; "repl-port"] in
-  Client.repl_port "LEIN_REPL_PORT" filename lein_port_err
+  match Client.repl_port "LEIN_REPL_PORT" filename with
+  | Some port -> port
+  | None -> Printf.eprintf "%s\n%!" lein_port_err;
+            Pervasives.exit 1
 
 let lein_ns = "leiningen.core.main"
 
